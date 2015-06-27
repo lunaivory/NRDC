4d1aec9ad2aa8c3d0a3adeed5233411a6fd3ce35
lunaivory <catlily234@gmail.com>
2015-06-27 11:56:22 +0800

    aggregate_match_patch skeleton

---
+++ aggregate_match_patch.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <opencv2/opencv>
+
+using namespace std;
+using namespace cv;
+
+namespace {
+
+double parLocal = 3;
+double parGlobal = 0.8;
+double parRatio = 0.5;
+double parSize = 500;
+double parSmall = 8;
+double parLarge = 64;
+
+} // namespace
+
+namespace nrdc {
+
+void AggregateMatchPatch() {
+//implement here
+
+}
+
+//other functions
+
+} // nrdc
---
+++ aggregate_match_patch.hpp
@@ -0,0 +1,8 @@
+#ifndef AGGREGATE_MATCH_PATCH_HPP
+#define AGGREGATE_MATCH_PATCH_HPP
+
+namespace nrdc {
+  AggregateMatchPatch(Mat src, Mat T);
+} // nrdc
+
+#endif //AGGREGATE_MATCH_PATCH_HPP
