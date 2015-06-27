678ebc7f0f3d40dea7ba86165b3ac76a16418087
Victor Hung <vhung@kth.se>
2015-06-27 23:29:20 +0800

    Commenting out some code due to compile errors

--- nrdc.cpp
+++ nrdc.cpp
@@ -1,4 +1,6 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
 #include "nearest_neighbor_search.hpp"
 // #include "aggregate_match_patch.hpp"
 // #include "global_color_transformation.hpp"
@@ -26,24 +28,25 @@ void load_images(Mat & source, Mat & reference){
 Mat nrdc(Mat src, Mat ref) {
   Mat srcS, refS;
 
-  for (var i = (1<<6); i > 0; i >>= 1) {
-    Size srcSz = src.size();
-    Size refSz = ref.size();
+  for (int i = (1<<6); i > 0; i >>= 1) {
+    // Size srcSz = src.size();
+    // Size refSz = ref.size();
 
-    srcSz.height /= i, srcSz.width /= i;
-    refSz.height /= i, srcSz.width /= i;
+    // srcSz.height /= i, srcSz.width /= i;
+    // refSz.height /= i, srcSz.width /= i;
 
-    resize(src, srcS, srcSz);
-    resize(ref, refS, refSz);
+    // resize(src, srcS, srcSz);
+    // resize(ref, refS, refSz);
 
-    NearestNeighborSearch();
+    nearest_neighbor_search(srcS, refS);
 
-    AggregateMatchPatch();
+    // AggregateMatchPatch();
 
-    GlobalColorTransformation();
+    // GlobalColorTransformation();
 
-    NarrowSearchRange();
+    // NarrowSearchRange();
   }
+  return refS;
 }
 
 int main(int argc, char const *argv[])
